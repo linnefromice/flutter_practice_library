9bfe1ba75e3d20a205fe9d9ec3e15f86f4acb15f
linnefromice <linnefromice@gmail.com>
2020-03-15 00:23:29 +0900

    add counter example of shared_preferences

--- lib/screen/SharedPreferencesScreen.dart
+++ lib/screen/SharedPreferencesScreen.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesScreen extends StatelessWidget {
+  final keyName = 'keyOne';
+
+  void _read() async {
+    final prefs = await SharedPreferences.getInstance();
+    final value = prefs.getInt(keyName) ?? 0;
+    print('read: $value');
+  }
+
+  _save() async {
+    final prefs = await SharedPreferences.getInstance();
+    var value = prefs.getInt(keyName) ?? 0;
+    value++;
+    prefs.setInt(keyName, value);
+    print('saved $value');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,7 +25,24 @@ class SharedPreferencesScreen extends StatelessWidget {
         title: Text("Practice - shared_preferences"),
       ),
       body: Center(
-        child: Text('Practice - shared_preferences')
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(8.0),
+              child: RaisedButton(
+                child: Text('SAVE'),
+                onPressed: _save,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(8.0),
+              child: RaisedButton(
+                child: Text('READ'),
+                onPressed: _read,
+              ),
+            )
+          ],
+        )
       ),
     );
   }
