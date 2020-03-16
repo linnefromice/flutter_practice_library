8d0bb78ccfd008d3f8ac1283c11aeaceeef607df
linnefromice <linnefromice@gmail.com>
2020-03-16 23:46:01 +0900

    modify SqfliteScreen - add logic using DatabaseHelper for operate sqflite

--- lib/screen/SqfliteScreen.dart
+++ lib/screen/SqfliteScreen.dart
@@ -1,6 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice_library/core/DatabaseHelper.dart';
 
 class SqfliteScreen extends StatelessWidget {
+
+  final dbHelper = DatabaseHelper.instance;
+
+  void _insert() async {
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnName: 'Mike',
+      DatabaseHelper.columnAge: 23
+    };
+    final id = await dbHelper.insert(row);
+    print('insert row id: $id');
+  }
+
+  void _query() async {
+    final allRows = await dbHelper.queryAllRows();
+    print('query all rows:');
+    allRows.forEach((row) => print(row));
+  }
+
+  void _update() async {
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnId: 1,
+      DatabaseHelper.columnName: 'harvey',
+      DatabaseHelper.columnAge: 30
+    };
+    final rowsAffected = await dbHelper.update(row);
+    print('update $rowsAffected row(s)');
+  }
+
+  void _delete() async {
+    final id = await dbHelper.queryRowCount();
+    final rowsDeleted = await dbHelper.delete(id);
+    print('deleted $rowsDeleted row(s): row $id');
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,9 +46,33 @@ class SqfliteScreen extends StatelessWidget {
           children: <Widget>[
             Text('Practice - sqflite"'),
             RaisedButton(
-              child: Text('BUTTON'),
-              onPressed: null,
-            )
+              child: Text(
+                'insert',
+                style: TextStyle(fontSize: 20),
+              ),
+              onPressed: _insert,
+            ),
+            RaisedButton(
+              child: Text(
+                'query',
+                style: TextStyle(fontSize: 20),
+              ),
+              onPressed: _query,
+            ),
+            RaisedButton(
+              child: Text(
+                'update',
+                style: TextStyle(fontSize: 20)
+              ),
+              onPressed: _update,
+            ),
+            RaisedButton(
+              child: Text(
+                'delete',
+                style: TextStyle(fontSize: 20)
+              ),
+              onPressed: _delete,
+            ),
           ],
         )
       ),
