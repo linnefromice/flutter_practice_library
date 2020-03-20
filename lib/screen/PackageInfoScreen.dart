b473e0391cb85ee296f2fba0a0200ebc930bc95a
linnefromice <linnefromice@gmail.com>
2020-03-20 22:23:23 +0900

    add PackageInfoScreen

---
+++ lib/screen/PackageInfoScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PackageInfoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Practice - package_info'),
+      ),
+      body: Center(
+        child: Text('Practice - package_info'),
+      ),
+    );
+  }
+}
