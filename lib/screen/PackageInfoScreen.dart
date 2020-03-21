b2a61b1edd0043319ec15bc1d2fbad45d1a6762b
linnefromice <linnefromice@gmail.com>
2020-03-21 23:12:06 +0900

    modify PackageInfoScreen - use package_info (temp)

--- lib/screen/PackageInfoScreen.dart
+++ lib/screen/PackageInfoScreen.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
 
 class PackageInfoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Practice - package_info'),
       ),
       body: Center(
-        child: Text('Practice - package_info'),
+        child: ListView(
+          children: <Widget>[
+            Text('Practice - package_info'),
+            /*
+            Card(
+              child: ListTile(
+                title: Text("App Name"),
+                subtitle: Text(packageInfo.appName),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Package Name"),
+                subtitle: Text(packageInfo.packageName),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Version"),
+                subtitle: Text(packageInfo.version),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                title: Text("Build Number"),
+                subtitle: Text(packageInfo.buildNumber),
+              ),
+            ),
+            */
+          ],
+        ),
       ),
     );
   }
