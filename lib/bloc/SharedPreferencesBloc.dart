889b559fcdfec852fcd1ea9445ae3fea419e1265
linnefromice <linnefromice@gmail.com>
2020-03-22 22:43:20 +0900

    update bloc for SharedPreferences

--- lib/bloc/SharedPreferencesBloc.dart
+++ lib/bloc/SharedPreferencesBloc.dart
@@ -1,9 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter_practice_library/core/SharedPreferencesWrapper.dart';
+
 class SharedPreferencesBloc {
+  String _key;
+  String _value;
+
+  final _inputKeyController = StreamController<String>();
+  Sink<String> get inputKey => _inputKeyController.sink;
+
+  final _inputValueController = StreamController<String>();
+  Sink<String> get inputValue => _inputValueController.sink;
+
+  final _saveController = StreamController<void>();
+  Sink<void> get requestSave => _saveController.sink;
+
   SharedPreferencesBloc() {
-    // TODO
+    _inputKeyController.stream.listen((val) {
+      _key = val;
+    });
+    _inputValueController.stream.listen((val) {
+      _value = val;
+    });
+    _saveController.stream.listen((_) {
+      SharedPreferencesWrapper.setString(_key, _value);
+    });
   }
 
   void dispose() {
-    // TODO
+    _inputKeyController.close();
+    _inputValueController.close();
+    _saveController.close();
   }
 }
