201b4eb672be80cb28bb7ff30cbe756d17d9e889
linnefromice <linnefromice@gmail.com>
2020-03-22 23:44:47 +0900

    modify PackageInfoScreen - add display of package_info

--- lib/screen/PackageInfoScreen.dart
+++ lib/screen/PackageInfoScreen.dart
@@ -2,45 +2,80 @@ import 'package:flutter/material.dart';
 import 'package:package_info/package_info.dart';
 
 class PackageInfoScreen extends StatelessWidget {
+  Widget _buildPackageInfo(PackageInfo packageInfo) {
+
+    List<Widget> _widgetList = [];
+    _widgetList.add(Text('Practice - package_info'));
+    if (packageInfo.appName != null) {
+      _widgetList.add(
+        Card(
+          child: ListTile(
+            title: Text("App Name"),
+            subtitle: Text(packageInfo.appName),
+          ),
+        )
+      );
+    }
+    if (packageInfo.packageName != null) {
+      _widgetList.add(
+        Card(
+          child: ListTile(
+            title: Text("Package Name"),
+            subtitle: Text(packageInfo.packageName),
+          ),
+        )
+      );
+    }
+    if (packageInfo.version != null) {
+      _widgetList.add(
+        Card(
+          child: ListTile(
+            title: Text("Version"),
+            subtitle: Text(packageInfo.version),
+          ),
+        )
+      );
+    }
+    if (packageInfo.buildNumber != null) {
+      _widgetList.add(
+        Card(
+          child: ListTile(
+            title: Text("Build Number"),
+            subtitle: Text(packageInfo.buildNumber),
+          ),
+        )
+      );
+    }
+    return ListView(
+      children: _widgetList
+    );
+  }
+
+  Widget _buildNoData() {
+    return ListView(
+      children: <Widget>[
+        Text('Practice - package_info'),
+        Text('NODATA'),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // PackageInfo packageInfo = await PackageInfo.fromPlatform();
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Practice - package_info'),
       ),
       body: Center(
-        child: ListView(
-          children: <Widget>[
-            Text('Practice - package_info'),
-            /*
-            Card(
-              child: ListTile(
-                title: Text("App Name"),
-                subtitle: Text(packageInfo.appName),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Package Name"),
-                subtitle: Text(packageInfo.packageName),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Version"),
-                subtitle: Text(packageInfo.version),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text("Build Number"),
-                subtitle: Text(packageInfo.buildNumber),
-              ),
-            ),
-            */
-          ],
+        child: FutureBuilder(
+          future: PackageInfo.fromPlatform(),
+          builder: (BuildContext context, AsyncSnapshot<PackageInfo> snapshot) {
+            if (snapshot.hasData) {
+              return _buildPackageInfo(snapshot.data);
+            } else {
+              return _buildNoData();
+            }
+          },
         ),
       ),
     );
