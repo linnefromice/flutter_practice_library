5cfea53cbf4f7016e8f4ace4b4e4d21dca3d3630
linnefromice <linnefromice@gmail.com>
2020-03-23 00:06:37 +0900

    modify DeviceInfoScreen

--- lib/screen/DeviceInfoScreen.dart
+++ lib/screen/DeviceInfoScreen.dart
@@ -1,15 +1,60 @@
+import 'dart:io';
+
+import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
 
 class DeviceInfoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Practice - device_info'),
-      ),
-      body: Center(
-        child: Text('Practice - device_info'),
-      ),
-    );
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    if (Platform.isAndroid) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Practice - device_info'),
+        ),
+        body: Center(
+          child: FutureBuilder(
+            future: deviceInfo.androidInfo,
+            builder: (BuildContext context, AsyncSnapshot<AndroidDeviceInfo> snapshot) {
+              if (snapshot.hasData) {
+                return ListView(
+                  children: <Widget>[
+                    Text("DEVICE"),
+                    Text(snapshot.data.display)
+                  ],
+                );
+              } else {
+                return Text('NODATA');
+              }
+            },
+          ),
+        ),
+      );
+    }
+    if (Platform.isIOS) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Practice - device_info'),
+        ),
+        body: Center(
+          child: FutureBuilder(
+            future: deviceInfo.iosInfo,
+            builder: (BuildContext context, AsyncSnapshot<IosDeviceInfo> snapshot) {
+              if (snapshot.hasData) {
+                return ListView(
+                  children: <Widget>[
+                    Text("DEVICE"),
+                    Text(snapshot.data.utsname.machine)
+                  ],
+                );
+              } else {
+                return Text('NODATA');
+              }
+            },
+          ),
+        ),
+      );
+    }
+    return Text("SYSTEM ERROR: Judgment impossible");
   }
 }
