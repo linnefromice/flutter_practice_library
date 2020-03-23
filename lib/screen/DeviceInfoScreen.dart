e0fcdb3261da69be37814b76eca3266a7c2fe2b9
linnefromice <linnefromice@gmail.com>
2020-03-23 11:11:46 +0900

    refactor - separate building device_info screen

--- lib/screen/DeviceInfoScreen.dart
+++ lib/screen/DeviceInfoScreen.dart
@@ -4,64 +4,73 @@ import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
 
 class DeviceInfoScreen extends StatelessWidget {
+  Widget _buildScaffoldForAndroid() {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Practice - device_info'),
+      ),
+      body: Center(
+        child: FutureBuilder(
+          future: deviceInfo.androidInfo,
+          builder: (BuildContext context, AsyncSnapshot<AndroidDeviceInfo> snapshot) {
+            if (snapshot.hasData) {
+              return ListView(
+                children: <Widget>[
+                  Card(
+                    child: ListTile(
+                      title: Text('DEVICE'),
+                      subtitle: Text(snapshot.data.display),
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return Text('NODATA');
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildScaffoldForIOS() {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Practice - device_info'),
+      ),
+      body: Center(
+        child: FutureBuilder(
+          future: deviceInfo.iosInfo,
+          builder: (BuildContext context, AsyncSnapshot<IosDeviceInfo> snapshot) {
+            if (snapshot.hasData) {
+              return ListView(
+                children: <Widget>[
+                  Card(
+                    child: ListTile(
+                      title: Text('DEVICE'),
+                      subtitle: Text(snapshot.data.utsname.machine),
+                    ),
+                  )
+                ],
+              );
+            } else {
+              return Text('NODATA');
+            }
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
     if (Platform.isAndroid) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Practice - device_info'),
-        ),
-        body: Center(
-          child: FutureBuilder(
-            future: deviceInfo.androidInfo,
-            builder: (BuildContext context, AsyncSnapshot<AndroidDeviceInfo> snapshot) {
-              if (snapshot.hasData) {
-                return ListView(
-                  children: <Widget>[
-                    Card(
-                      child: ListTile(
-                        title: Text('DEVICE'),
-                        subtitle: Text(snapshot.data.display),
-                      ),
-                    ),
-                  ],
-                );
-              } else {
-                return Text('NODATA');
-              }
-            },
-          ),
-        ),
-      );
+      return _buildScaffoldForAndroid();
     }
     if (Platform.isIOS) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Practice - device_info'),
-        ),
-        body: Center(
-          child: FutureBuilder(
-            future: deviceInfo.iosInfo,
-            builder: (BuildContext context, AsyncSnapshot<IosDeviceInfo> snapshot) {
-              if (snapshot.hasData) {
-                return ListView(
-                  children: <Widget>[
-                    Card(
-                      child: ListTile(
-                        title: Text('DEVICE'),
-                        subtitle: Text(snapshot.data.utsname.machine),
-                      ),
-                    )
-                  ],
-                );
-              } else {
-                return Text('NODATA');
-              }
-            },
-          ),
-        ),
-      );
+      return _buildScaffoldForIOS();
     }
     return Text("SYSTEM ERROR: Judgment impossible");
   }
