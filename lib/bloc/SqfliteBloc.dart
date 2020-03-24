7d58083f6cd2dc24cb93e70695067edf929741bf
linnefromice <linnefromice@gmail.com>
2020-03-24 23:05:15 +0900

    create bloc for sqflite - create StreamController (no logic)

--- lib/bloc/SqfliteBloc.dart
+++ lib/bloc/SqfliteBloc.dart
@@ -1,9 +1,27 @@
+import 'dart:async';
+
 class SqfliteBloc {
+
+  final _insertController = StreamController<void>();
+  Sink<void> get requestInsert => _insertController.sink;
+
+  final _queryController = StreamController<void>();
+  Sink<void> get requestQuery => _queryController.sink;
+
+  final _updateController = StreamController<void>();
+  Sink<void> get requestUpdate => _updateController.sink;
+
+  final _deleteController = StreamController<void>();
+  Sink<void> get requestDelete => _deleteController.sink;
+
   SqfliteBloc() {
     // TODO
   }
 
   void dispose() {
-    // TODO
+    _insertController.close();
+    _queryController.close();
+    _updateController.close();
+    _deleteController.close();
   }
 }
