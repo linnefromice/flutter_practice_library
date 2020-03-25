cb9d4671074ad73cf88699d50d9c681816dd16d3
linnefromice <linnefromice@gmail.com>
2020-03-25 22:34:51 +0900

    modify SqfliteScreen - modify using bloc

--- lib/screen/SqfliteScreen.dart
+++ lib/screen/SqfliteScreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice_library/bloc/SqfliteBloc.dart';
-import 'package:flutter_practice_library/core/DatabaseHelper.dart';
 import 'package:provider/provider.dart';
 
 class SqfliteScreen extends StatelessWidget {
@@ -16,40 +15,9 @@ class SqfliteScreen extends StatelessWidget {
 
 class _Screen extends StatelessWidget {
 
-  final dbHelper = DatabaseHelper.instance;
-
-  void _insert() async {
-    Map<String, dynamic> row = {
-      DatabaseHelper.columnName: 'Mike',
-      DatabaseHelper.columnAge: 23
-    };
-    final id = await dbHelper.insert(row);
-    print('insert row id: $id');
-  }
-
-  void _query() async {
-    final allRows = await dbHelper.queryAllRows();
-    print('query all rows:');
-    allRows.forEach((row) => print(row));
-  }
-
-  void _update() async {
-    Map<String, dynamic> row = {
-      DatabaseHelper.columnId: 1,
-      DatabaseHelper.columnName: 'harvey',
-      DatabaseHelper.columnAge: 30
-    };
-    final rowsAffected = await dbHelper.update(row);
-    print('update $rowsAffected row(s)');
-  }
-
-  void _delete() async {
-    final id = await dbHelper.queryRowCount();
-    final rowsDeleted = await dbHelper.delete(id);
-    print('deleted $rowsDeleted row(s): row $id');
-  }
-
   Widget build(BuildContext context) {
+    final bloc = Provider.of<SqfliteBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Practice - sqflite"),
@@ -63,28 +31,28 @@ class _Screen extends StatelessWidget {
                 'insert',
                 style: TextStyle(fontSize: 20),
               ),
-              onPressed: _insert,
+              onPressed: () => bloc.requestInsert.add(null),
             ),
             RaisedButton(
               child: Text(
                 'query',
                 style: TextStyle(fontSize: 20),
               ),
-              onPressed: _query,
+              onPressed: () => bloc.requestQuery.add(null),
             ),
             RaisedButton(
               child: Text(
                 'update',
                 style: TextStyle(fontSize: 20)
               ),
-              onPressed: _update,
+              onPressed: () => bloc.requestUpdate.add(null),
             ),
             RaisedButton(
               child: Text(
                 'delete',
                 style: TextStyle(fontSize: 20)
               ),
-              onPressed: _delete,
+              onPressed: () => bloc.requestDelete.add(null),
             ),
           ],
         )
