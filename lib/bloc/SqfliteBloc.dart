6f5ab3e8e47041218090cc01a10d03529fc67d9b
linnefromice <linnefromice@gmail.com>
2020-03-25 22:28:26 +0900

    update bloc for sqflite - create logic when listening

--- lib/bloc/SqfliteBloc.dart
+++ lib/bloc/SqfliteBloc.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 
+import 'package:flutter_practice_library/core/DatabaseHelper.dart';
+
 class SqfliteBloc {
+  final dbHelper = DatabaseHelper.instance;
 
   final _insertController = StreamController<void>();
   Sink<void> get requestInsert => _insertController.sink;
@@ -15,7 +18,46 @@ class SqfliteBloc {
   Sink<void> get requestDelete => _deleteController.sink;
 
   SqfliteBloc() {
-    // TODO
+    _insertController.stream.listen((_) {
+      _insert();
+    });
+    _queryController.stream.listen((_) {
+      _query();
+    });
+    _updateController.stream.listen((_) {
+      _update();
+    });
+    _deleteController.stream.listen((_) {
+      _delete();
+    });
+  }
+
+  void _insert() async {
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnName: 'Mike',
+      DatabaseHelper.columnAge: 23
+    };
+    final id = await dbHelper.insert(row);
+    print('insert row id: $id');
+  }
+  void _query() async {
+    final allRows = await dbHelper.queryAllRows();
+    print('query all rows:');
+    allRows.forEach((row) => print(row));
+  }
+  void _update() async {
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnId: 1,
+      DatabaseHelper.columnName: 'harvey',
+      DatabaseHelper.columnAge: 30
+    };
+    final rowsAffected = await dbHelper.update(row);
+    print('update $rowsAffected row(s)');
+  }
+  void _delete() async {
+    final id = await dbHelper.queryRowCount();
+    final rowsDeleted = await dbHelper.delete(id);
+    print('deleted $rowsDeleted row(s): row $id');
   }
 
   void dispose() {
