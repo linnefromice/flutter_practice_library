ddf7c42665a9023be47863f617a6ad339629c464
linnefromice <linnefromice@gmail.com>
2020-03-26 10:46:40 +0900

    add bloc to DioScreen

--- lib/screen/DioScreen.dart
+++ lib/screen/DioScreen.dart
@@ -1,8 +1,20 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_practice_library/bloc/DioBloc.dart';
+import 'package:provider/provider.dart';
 
 class DioScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider<DioBloc>(
+      create: (context) => DioBloc(),
+      dispose: (context, bloc) => bloc.dispose(),
+      child: _Screen(),
+    );
+  }
+}
 
+class _Screen extends StatelessWidget {
   _request() async {
     final dio = Dio();
     Response response = await dio.get('https://jsonplaceholder.typicode.com/posts/1');
@@ -13,13 +25,13 @@ class DioScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Practice - dio')
+          title: Text('Practice - dio')
       ),
       body: Center(
-        child: RaisedButton(
-          child: Text('REQUEST'),
-          onPressed: _request,
-        )
+          child: RaisedButton(
+            child: Text('REQUEST'),
+            onPressed: _request,
+          )
       ),
     );
   }
