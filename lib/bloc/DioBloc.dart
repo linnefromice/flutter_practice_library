5784b2769cec465a814abe99f8b58e5ffe7e96a0
linnefromice <linnefromice@gmail.com>
2020-03-28 06:34:28 +0900

    modify DioBloc - add StreamController (sample logic)

--- lib/bloc/DioBloc.dart
+++ lib/bloc/DioBloc.dart
@@ -1,9 +1,24 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+
 class DioBloc {
+  final _requestApiController = StreamController<void>();
+  Sink<void> get requestApi => _requestApiController.sink;
+
   DioBloc() {
-    // TODO
+    _requestApiController.stream.listen((_) {
+      _request();
+    });
+  }
+
+  _request() async {
+    final dio = Dio();
+    Response response = await dio.get('https://jsonplaceholder.typicode.com/posts/1');
+    print(response.data);
   }
 
   void dispose() {
-    // TODO
+    _requestApiController.close();
   }
 }
