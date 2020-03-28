6819b159342eaea166dff92e059199ca1755bef0
linnefromice <linnefromice@gmail.com>
2020-03-28 16:07:55 +0900

    add DioBloc - add field for id using request api

--- lib/bloc/DioBloc.dart
+++ lib/bloc/DioBloc.dart
@@ -4,15 +4,23 @@ import 'package:dio/dio.dart';
 import 'package:flutter_practice_library/model/jsonplaceholder/Post.dart';
 
 class DioBloc {
+  String _inputedId;
+
   final _requestApiController = StreamController<void>();
   Sink<void> get requestApi => _requestApiController.sink;
 
   final _responseApiController = StreamController<List<Post>>();
   Stream<List<Post>> get responseApi => _responseApiController.stream;
 
+  final _inputedIdController = StreamController<String>();
+  Sink<String> get inputId => _inputedIdController.sink;
+
   DioBloc() {
     _requestApiController.stream.listen((_) {
       _request();
+    });
+    _inputedIdController.stream.listen((val) {
+      _inputedId = val;
     });
   }
 
@@ -27,5 +35,6 @@ class DioBloc {
   void dispose() {
     _requestApiController.close();
     _responseApiController.close();
+    _inputedIdController.close();
   }
 }
