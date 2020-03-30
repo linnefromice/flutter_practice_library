1a8852f7d00ab1b7c2ae3cf4bd9a41802e1a6cf9
linnefromice <linnefromice@gmail.com>
2020-03-30 22:28:38 +0900

    modify PracticeBlocOne - add StreamController for api request&response (not used)

--- lib/bloc/practice/PracticeBlocOne.dart
+++ lib/bloc/practice/PracticeBlocOne.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:dio/dio.dart';
+
 class PracticeOneBloc {
   String _inputedType;
 
@@ -7,14 +9,30 @@ class PracticeOneBloc {
   Sink<String> get inputType => _inputedTypeController.sink;
   Stream<String> get getType => _inputedTypeController.stream;
 
+  final _requestApiController = StreamController<String>();
+  Sink<String> get requestApi => _requestApiController.sink;
+
+  final _responseApiController = StreamController<String>();
+  Stream<String> get responseApi => _responseApiController.stream;
+
   PracticeOneBloc() {
     _inputedTypeController.stream.listen((val) {
       _inputedType = val;
       _inputedTypeController.sink.add(_inputedType);
     });
+    _requestApiController.stream.listen((val) {
+      _request(val);
+    });
+  }
+
+  void _request(final String value) async {
+    final Response response = await Dio().get('https://jsonplaceholder.typicode.com/' + value + '/1');
+    _responseApiController.sink.add(response.data.toString());
   }
 
   void dispose() {
     _inputedTypeController.close();
+    _requestApiController.close();
+    _responseApiController.close();
   }
 }
