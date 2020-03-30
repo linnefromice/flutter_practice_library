48b9092f9e224176e43500a1eb9917c22836fe08
linnefromice <linnefromice@gmail.com>
2020-03-30 22:11:36 +0900

    refactor - separate drawer/body parts

--- lib/screen/MainScreen.dart
+++ lib/screen/MainScreen.dart
@@ -96,121 +96,129 @@ class MainScreen extends StatelessWidget {
     );
   }
 
+  Widget _buildDrawer(final BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: ListTile(
+              title: Text('Dummy Pages'),
+              subtitle: Text('For Practice'),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            title: Text('PracticeBlocAndStatefulWidget'),
+            subtitle: Text('bloc X StatefulWidget'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PracticeBlocAndStatefulWidgetScreen()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBody(final BuildContext context) {
+    return Center(
+      child: ListView(
+        children: <Widget>[
+          _buildLibraryInformationCard(
+              'shared_preferences',
+              'Wraps NSUserDefaults (on iOS) and SharedPreferences (on Android).',
+              'https://pub.dev/packages/shared_preferences',
+              context,
+              SharedPreferencesScreen()
+          ),
+          _buildLibraryInformationCard(
+              'sqflite',
+              'SQLite plugin for Flutter',
+              'https://pub.dev/packages/sqflite',
+              context,
+              SqfliteScreen()
+          ),
+          _buildLibraryInformationCard(
+              'dio',
+              'A powerful Http client for Dart',
+              'https://pub.dev/packages/dio',
+              context,
+              DioScreen()
+          ),
+          _buildLibraryInformationCard(
+              'package_info',
+              'This Flutter plugin provides an API for querying information about an application package.',
+              'https://pub.dev/packages/package_info',
+              context,
+              PackageInfoScreen()
+          ),
+          _buildLibraryInformationCard(
+              'device_info',
+              'Get current device information from within the Flutter application.',
+              'https://pub.dev/packages/device_info',
+              context,
+              DeviceInfoScreen()
+          ),
+          _buildLibraryInformationCard(
+            'flutter_webview_plugin',
+            'Plugin that allows Flutter to communicate with a native WebView.',
+            'https://pub.dev/packages/flutter_webview_plugin',
+            context,
+            FlutterWebviewPluginScreen(), // dummy
+          ),
+          _buildLibraryInformationCard(
+            'flutter_swiper',
+            'The best swiper for flutter , with multiple layouts, infinite loop. Compatible with Android & iOS.',
+            'https://pub.dev/packages/flutter_swiper',
+            context,
+            FlutterSwiperScreen(), // dummy
+          ),
+          _buildLibraryInformationCard(
+            'connectivity',
+            'This plugin allows Flutter apps to discover network connectivity and configure themselves accordingly.',
+            'https://pub.dev/packages/connectivity',
+            context,
+            MainScreen(), // dummy
+          ),
+          _buildLibraryInformationCard(
+            'rxdart',
+            'RxDart adds additional capabilities to Dart Streams and StreamControllers.',
+            'https://pub.dev/packages/rxdart',
+            context,
+            MainScreen(), // dummy
+          ),
+          _buildLibraryInformationCard(
+            'built_value',
+            'Built Values for Dart',
+            'https://pub.dev/packages/built_value',
+            context,
+            MainScreen(), // dummy
+          ),
+          _buildLibraryInformationCard(
+            'battery',
+            'A Flutter plugin to access various information about the battery of the device the app is running on.',
+            'https://pub.dev/packages/battery',
+            context,
+            MainScreen(), // dummy
+          ),
+        ]
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter Practice Library"),
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: ListTile(
-                title: Text('Dummy Pages'),
-                subtitle: Text('For Practice'),
-              ),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-            ),
-            ListTile(
-              title: Text('PracticeBlocAndStatefulWidget'),
-              subtitle: Text('bloc X StatefulWidget'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PracticeBlocAndStatefulWidgetScreen()),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-      body: Center(
-        child: ListView(
-          children: <Widget>[
-            _buildLibraryInformationCard(
-              'shared_preferences',
-              'Wraps NSUserDefaults (on iOS) and SharedPreferences (on Android).',
-              'https://pub.dev/packages/shared_preferences',
-              context,
-              SharedPreferencesScreen()
-            ),
-            _buildLibraryInformationCard(
-              'sqflite',
-              'SQLite plugin for Flutter',
-              'https://pub.dev/packages/sqflite',
-              context,
-              SqfliteScreen()
-            ),
-            _buildLibraryInformationCard(
-              'dio',
-              'A powerful Http client for Dart',
-              'https://pub.dev/packages/dio',
-              context,
-              DioScreen()
-            ),
-            _buildLibraryInformationCard(
-              'package_info',
-              'This Flutter plugin provides an API for querying information about an application package.',
-              'https://pub.dev/packages/package_info',
-              context,
-              PackageInfoScreen()
-            ),
-            _buildLibraryInformationCard(
-              'device_info',
-              'Get current device information from within the Flutter application.',
-              'https://pub.dev/packages/device_info',
-              context,
-              DeviceInfoScreen()
-            ),
-            _buildLibraryInformationCard(
-              'flutter_webview_plugin',
-              'Plugin that allows Flutter to communicate with a native WebView.',
-              'https://pub.dev/packages/flutter_webview_plugin',
-              context,
-              FlutterWebviewPluginScreen(), // dummy
-            ),
-            _buildLibraryInformationCard(
-              'flutter_swiper',
-              'The best swiper for flutter , with multiple layouts, infinite loop. Compatible with Android & iOS.',
-              'https://pub.dev/packages/flutter_swiper',
-              context,
-              FlutterSwiperScreen(), // dummy
-            ),
-            _buildLibraryInformationCard(
-              'connectivity',
-              'This plugin allows Flutter apps to discover network connectivity and configure themselves accordingly.',
-              'https://pub.dev/packages/connectivity',
-              context,
-              MainScreen(), // dummy
-            ),
-            _buildLibraryInformationCard(
-              'rxdart',
-              'RxDart adds additional capabilities to Dart Streams and StreamControllers.',
-              'https://pub.dev/packages/rxdart',
-              context,
-              MainScreen(), // dummy
-            ),
-            _buildLibraryInformationCard(
-              'built_value',
-              'Built Values for Dart',
-              'https://pub.dev/packages/built_value',
-              context,
-              MainScreen(), // dummy
-            ),
-            _buildLibraryInformationCard(
-              'battery',
-              'A Flutter plugin to access various information about the battery of the device the app is running on.',
-              'https://pub.dev/packages/battery',
-              context,
-              MainScreen(), // dummy
-            ),
-          ]
-        )
-      ),
+      drawer: _buildDrawer(context),
+      body: _buildBody(context),
     );
   }
 }
