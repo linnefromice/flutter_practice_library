486070dbfd8bd12350813bff8b69b8eb977dd5c9
linnefromice <linnefromice@gmail.com>
2020-04-03 19:28:10 +0900

    add model - user of JSONPlaceholder

---
+++ lib/model/jsonplaceholder/User.dart
@@ -0,0 +1,141 @@
+// To parse this JSON data, do
+//
+//     final user = userFromJson(jsonString);
+
+import 'dart:convert';
+
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final Address address;
+  final String phone;
+  final String website;
+  final Company company;
+
+  User({
+    this.id,
+    this.name,
+    this.username,
+    this.email,
+    this.address,
+    this.phone,
+    this.website,
+    this.company,
+  });
+
+  factory User.fromRawJson(String str) => User.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+    id: json["id"],
+    name: json["name"],
+    username: json["username"],
+    email: json["email"],
+    address: Address.fromJson(json["address"]),
+    phone: json["phone"],
+    website: json["website"],
+    company: Company.fromJson(json["company"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "username": username,
+    "email": email,
+    "address": address.toJson(),
+    "phone": phone,
+    "website": website,
+    "company": company.toJson(),
+  };
+}
+
+class Address {
+  final String street;
+  final String suite;
+  final String city;
+  final String zipcode;
+  final Geo geo;
+
+  Address({
+    this.street,
+    this.suite,
+    this.city,
+    this.zipcode,
+    this.geo,
+  });
+
+  factory Address.fromRawJson(String str) => Address.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Address.fromJson(Map<String, dynamic> json) => Address(
+    street: json["street"],
+    suite: json["suite"],
+    city: json["city"],
+    zipcode: json["zipcode"],
+    geo: Geo.fromJson(json["geo"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "street": street,
+    "suite": suite,
+    "city": city,
+    "zipcode": zipcode,
+    "geo": geo.toJson(),
+  };
+}
+
+class Geo {
+  final String lat;
+  final String lng;
+
+  Geo({
+    this.lat,
+    this.lng,
+  });
+
+  factory Geo.fromRawJson(String str) => Geo.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Geo.fromJson(Map<String, dynamic> json) => Geo(
+    lat: json["lat"],
+    lng: json["lng"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "lat": lat,
+    "lng": lng,
+  };
+}
+
+class Company {
+  final String name;
+  final String catchPhrase;
+  final String bs;
+
+  Company({
+    this.name,
+    this.catchPhrase,
+    this.bs,
+  });
+
+  factory Company.fromRawJson(String str) => Company.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Company.fromJson(Map<String, dynamic> json) => Company(
+    name: json["name"],
+    catchPhrase: json["catchPhrase"],
+    bs: json["bs"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "name": name,
+    "catchPhrase": catchPhrase,
+    "bs": bs,
+  };
+}
