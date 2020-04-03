3ef7cf052710c0e5dccf39f2ad1f89a7ef32bebd
linnefromice <linnefromice@gmail.com>
2020-04-03 22:02:59 +0900

    modify DioScreen - add display for Photo

--- lib/screen/DioScreen.dart
+++ lib/screen/DioScreen.dart
@@ -3,8 +3,10 @@ import 'package:flutter/services.dart';
 import 'package:flutter_practice_library/bloc/DioBloc.dart';
 import 'package:flutter_practice_library/model/jsonplaceholder/Album.dart';
 import 'package:flutter_practice_library/model/jsonplaceholder/Comment.dart';
+import 'package:flutter_practice_library/model/jsonplaceholder/Photo.dart';
 import 'package:flutter_practice_library/model/jsonplaceholder/Post.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DioScreen extends StatelessWidget {
   @override
@@ -32,6 +34,12 @@ class _State extends State<_Screen> {
     'users':'ユーザ'
   };
   String _dropdownValue = 'posts';
+
+  void _launchUrl(final String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
+  }
 
   Widget _buildRequestButton(final DioBloc bloc) {
     return RaisedButton(
@@ -150,6 +158,32 @@ class _State extends State<_Screen> {
     return widgets;
   }
 
+  List<Widget> _buildPhotoCards(final List<Photo> entries) {
+    List<Widget> widgets = [];
+    widgets.add(
+      Text(
+        'Photo',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic,
+        ),
+      )
+    );
+    entries.forEach((dto) => widgets.add(
+      Card(
+        child: ListTile(
+          leading: Text(dto.id.toString()),
+          title: Text(dto.title),
+          trailing: RaisedButton.icon(
+            onPressed: () => _launchUrl(dto.url),
+            icon: Icon(Icons.link),
+            label: Text('Link'),
+          ),
+        ),
+      )
+    ));
+    return widgets;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -192,6 +226,8 @@ class _State extends State<_Screen> {
                         widgets = _buildCommentCards(dtos);
                       } else if (dtos[0] is Album) {
                         widgets = _buildAlbumCards(dtos);
+                      } else if (dtos[0] is Photo) {
+                        widgets = _buildPhotoCards(dtos);
                       } else {
                         dtos.forEach((v) {
                           widgets.add(Text(v.toString()));
